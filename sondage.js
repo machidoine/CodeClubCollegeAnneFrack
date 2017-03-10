92690ad8ee57dd06d3d122e8d347d4a09dba4502
thomas5962 <thomas.weispecher59@gmail.com>
2017-03-10 12:36:56 +0100

    commit initial

--- sondage.js
+++ sondage.js
@@ -1,1 +1,8 @@
-﻿
+﻿(document).ready(function(){
+	$("#send").click(function(){
+		//traitement
+		console.log("j'ai cliqué");
+        console.log($("#message4").val());
+		$("#content").val($("#message4").val());
+	});
+});
