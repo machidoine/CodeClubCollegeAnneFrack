2a24de96231a8923ab215f189579523d16fbf5f1
Kantin <kantin.desmettre@gmail.com>
2017-03-31 13:45:32 +0200

    mofification html pour bouton

--- sondage.js
+++ sondage.js
@@ -1,8 +1,11 @@
-﻿(document).ready(function(){
+﻿$(document).ready(function(){
 	$("#send").click(function(){
 		//traitement
 		console.log("j'ai cliqué");
         console.log($("#message4").val());
 		$("#content").val($("#message4").val());
+	});
+	$("#addButton").click(function(){
+	    //traitement ;
 	});
 });
