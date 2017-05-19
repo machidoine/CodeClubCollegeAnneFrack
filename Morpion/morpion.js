c5e7c0d7624c27e772472c7b397cb04bdf5e46b7
thomas <thomas@gmail.com>
2017-05-19 13:04:36 +0200

    ajouttml

--- Morpion/morpion.js
+++ Morpion/morpion.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	var symbole="O";
 	$("td").click(function(e){
 		$(e.currentTarget).html(symbole);
+		var v= $(e.currentTarget).html();
+		console.log(v);
 		if(symbole==="X"){
 			symbole="O";
 		}
